1d74fbebb98bccd32f4d9c6b98c380174b965f0a
Kevin <kopp020@gmail.com>
2018-09-17 19:34:01 -0400

    Update Root.vue

--- src/components/Root.vue
+++ src/components/Root.vue
@@ -18,7 +18,20 @@ export default {
            
          }
        },
-
+         computed: {
+    fullName: {
+      // getter
+      get: function () {
+        return this.firstName + ' ' + this.lastName
+      },
+      // setter
+      set: function (newValue) {
+        var names = newValue.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[names.length - 1]
+      }
+    }
+  },
        methods: {
        
          ಠ(){
